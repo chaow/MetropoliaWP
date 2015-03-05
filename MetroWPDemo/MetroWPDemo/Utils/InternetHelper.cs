d28b4ea43bbec1838b7a2412016849871d62b5d3
Chao Wei <chao.wei@aalto.fi>
2015-03-05 19:08:56 +0200

    adding internet helper

---
+++ MetroWPDemo/MetroWPDemo/Utils/InternetHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroWPDemo.Utils
+{
+    public class InternetHelper
+    {
+        public static bool isAvailable()
+        {
+            bool isConnected = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+            if (!isConnected)
+            {
+                // No internet connection is avaliable. The full functionality of the app isn't avaliable.
+            }
+            else
+            {
+                Windows.Networking.Connectivity.ConnectionProfile InternetConnectionProfile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+                Windows.Networking.Connectivity.NetworkConnectivityLevel connection = InternetConnectionProfile.GetNetworkConnectivityLevel();
+                if (connection == Windows.Networking.Connectivity.NetworkConnectivityLevel.None || connection == Windows.Networking.Connectivity.NetworkConnectivityLevel.LocalAccess)
+                {
+                    isConnected = false;
+                }
+            }
+            return isConnected;
+        }
+    }
+}
